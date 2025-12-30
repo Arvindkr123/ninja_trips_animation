befd4deff6ad945e268cecd67c2741c63665c02a
Arvindkr123 <thakurarvindkr10@gmail.com>
2025-12-30 13:02:56 +0530

    added starter code for ninja trips

---
+++ lib/screens/details.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+import 'package:ninja_trips/models/Trip.dart';
+import 'package:ninja_trips/shared/heart.dart';
+
+class Details extends StatelessWidget {
+  final Trip trip;
+  const Details({Key? key, required this.trip}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    const loremIpsum =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. "
+        "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
+        "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.";
+    return Scaffold(
+      appBar: AppBar(backgroundColor: Colors.transparent, elevation: 0),
+      extendBodyBehindAppBar: true,
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset('images/${trip.img}', height: 360, fit: BoxFit.cover),
+            const SizedBox(height: 30),
+            ListTile(
+              title: Text(
+                trip.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                  color: Colors.grey[800],
+                ),
+              ),
+              subtitle: Text(
+                '${trip.nights} night stay for only \$${trip.price}',
+              ),
+              trailing: Heart(),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(18),
+              child: Text(
+                loremIpsum,
+                style: TextStyle(color: Colors.grey[600], height: 1.4),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/shared/tripList.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:ninja_trips/models/Trip.dart';
+import 'package:ninja_trips/screens/details.dart';
+
+class TripList extends StatefulWidget {
+  const TripList({super.key});
+
+  @override
+  State<TripList> createState() => _TripListState();
+}
+
+class _TripListState extends State<TripList> {
+  final List<Widget> _tripTiles = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadTrips();
+  }
+
+  void _loadTrips() {
+    final List<Trip> trips = [
+      Trip(title: 'Beach Paradise', price: '350', nights: '3', img: 'beach.png'),
+      Trip(title: 'City Break', price: '400', nights: '5', img: 'city.png'),
+      Trip(title: 'Ski Adventure', price: '750', nights: '2', img: 'ski.png'),
+      Trip(title: 'Space Blast', price: '600', nights: '4', img: 'space.png'),
+    ];
+
+    for (final trip in trips) {
+      _tripTiles.add(_buildTile(trip));
+    }
+  }
+
+  Widget _buildTile(Trip trip) {
+    return ListTile(
+      contentPadding: const EdgeInsets.all(10),
+      leading: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: Image.asset(
+          'images/${trip.img}',
+          height: 50,
+          width: 50,
+          fit: BoxFit.cover,
+        ),
+      ),
+      title: Text(
+        trip.title,
+        style: TextStyle(fontSize: 20, color: Colors.grey[700]),
+      ),
+      subtitle: Text(
+        '${trip.nights} nights',
+        style: TextStyle(
+          fontSize: 14,
+          fontWeight: FontWeight.bold,
+          color: Colors.blue[300],
+        ),
+      ),
+      trailing: Text('\$${trip.price}'),
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>Details(trip:trip)));
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: _tripTiles.length,
+      itemBuilder: (context, index){
+        return _tripTiles[index];
+      }
+    );
+  }
+}
